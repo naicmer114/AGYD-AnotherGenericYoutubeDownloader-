b6f5a02855a51cbb406aec60e949032f23c68b3d
naicmer114 <sergiojoseriveromarquez@gmail.com>
2026-02-08 11:23:26 -0400

    Un-Yanderedev code
    Revamp of code and documentation of it.
    Now the GUI is more simple, in the future it will have more advance options.
    The process for video  and audio on down.py is more clear.
    For the use of only the code it have to be the full build of ffmpeg.

--- down.py
+++ down.py
@@ -1,45 +1,86 @@
 from yt_dlp import YoutubeDL
-from ffmpeg import *
-calidadL = ["Baja", "Alta"]
-audioC = ["worstaudio", "bestaudio"]
-videoC = ["worst", "best"]
 
-def to_quality(calidad):
-    for i in range(2):
-            print(i)
-            if calidadL[i] == calidad:   
-                print(calidad + " Es igual a " + calidadL[i])
-                print("calidad actual: " + calidad)
-                return int(i)
-                
-def download(url, ruta, video, formato, calidad):
-    cal_vid = videoC[1] # la calidad del video siempre sera la maxima
-    if formato != "": # Para audio no predeterminado
-        calidad = audioC[to_quality(calidad)]
-    else: # Para video
-        if(video == 1):
-            formato = "mp4"
-            conf_global = int(to_quality(calidad))
-            calidad = audioC[conf_global]
-            cal_vid = videoC[conf_global]
-        else: # Predeterminado
-            formato = "m4a"
-    ydl_opts = {
-    'format': formato+'/'+calidad+'/'+cal_vid,
-    'outtmpl': ruta+'/Track: %(playlist_index)s - %(title)s.%(ext)s', #%(uploader)s - %(album)s - 
-    'quiet': True, # Desactiva el output a consola
-    'writethumbnail': True, # Se baja la portada
-    'postprocessors': [  # Extract audio using ffmpeg
-        {'key': 'FFmpegExtractAudio', # Separacion de audio del video para posprocesado 
-         'preferredcodec': formato}, # Codec de audio/video a usar
-        {'key': 'FFmpegMetadata'}, # Se incorpora la metadata de la cancion. Se hace primero ya que la portada se pierde al reescribir la metadata, mas no al revez
-        {'key': 'EmbedThumbnail'}, # Se incorpora la portada al archivo
-    ]
-    }
-    print("> Descargando")
-    print(ydl_opts)
-    print("-----------------------------")
-    YoutubeDL(ydl_opts).download(url) # Usando el metodo YoutubeDL y dandole las configuraciones previas como parametros, se emplea su comportameniento de descargar (Download) dandole el url del video / playlist
-    #print("> Finalizado -- wwwww --")
-    #print("> Finalizado --  ///  --")
-    #print("> Finalizado   wwwwwww  ")
+import os
+import sys
+
+
+# Search resources function
+def resource_path(name):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, name)
+    return os.path.join(os.path.abspath("."), name)
+
+
+# Se declaran las rutas de ffmpeg y ffprobe
+ffmpeg_dir = os.path.dirname(resource_path("ffmpeg.exe"))
+ffprobe_dir = os.path.dirname(resource_path("ffprobe.exe"))
+# print("ffmpeg:", ffmpeg_dir, os.path.exists(ffmpeg_dir))
+# print("ffprobe:", ffprobe_dir, os.path.exists(ffprobe_dir))
+# print("PATH:", os.environ["PATH"])
+
+
+# Lists of quality presets
+Quality = ["worst", "best"]
+Quality_list_for_audio = ["worstaudio", "bestaudio"]
+Quality_list_for_video = ["worstvideo", "bestvideo"]
+
+
+def download(url, ruta, is_video, formato, calidad_general):
+    # -- Declaro la variable para posterior uso
+    Quality_global = Quality[calidad_general]
+    Quality_audio = ""
+    Quality_vid = ""
+
+    # -- PROCESADO DE ydl_opts
+    # PARA AUDIO, con inyection de portadas y metadata
+    if is_video == 0:
+        Quality_audio = Quality_list_for_audio[calidad_general]
+        ydl_opts = {
+            "format": Quality_global,
+            "outtmpl": ruta + "/Track: %(playlist_index)s - %(title)s.%(ext)s",
+            "quiet": True,  # Desactiva el output a consola
+            "ffmpeg_location": ffmpeg_dir,
+            "postprocessors": [  # Extract audio using ffmpeg
+                {
+                    # Separacion de audio del video para posprocesado
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": formato,  # Codec de audio/video a usar
+                    "preferredquality": "320",
+                    "nopostoverwrites": False,
+                },
+                {
+                    # Se incorpora la metadata de la cancion.
+                    # Se hace primero ya que la portada se pierde al reescribir
+                    # la metadata, mas no al revez
+                    "key": "FFmpegMetadata"
+                },
+                {"key": "EmbedThumbnail"},  # Se incorpora la portada al archivo
+            ],
+            "writethumbnail": True,  # Se baja la portada
+            "verbose": True,
+        }
+
+    # PARA VIDEO, con inyection de portadas y metadata
+    if is_video == 1:
+        Quality_audio = Quality_list_for_audio[calidad_general]
+        Quality_vid = Quality_list_for_video[calidad_general]
+
+        ydl_opts = {
+            "format": formato + "/" + Quality_audio + "/" + Quality_vid,
+            "outtmpl": ruta + "/Track: %(playlist_index)s - %(title)s.%(ext)s",
+            "quiet": True,  # Desactiva el output a consola
+            "ffmpeg_location": os.path.realpath("./libs/ffmpeg/bin/ffmpeg.exe"),
+        }
+
+    org_std = sys.stdout  # save the original sys.stdout to reset it
+    with open("out.txt", "a") as f:
+        sys.stdout = f  # redirect stdout to direct write
+        """
+            Usando el metodo YoutubeDL y dandole las configuraciones previas
+            como parametros, se emplea su comportameniento de descargar
+            (Download) dandole el url del video / playlist
+        """
+        print("> Descargando")
+        print(ydl_opts)
+        YoutubeDL(ydl_opts).download(url)
+    sys.stdout = org_std  # reset the stdout as original as it was
--- front.py
+++ front.py
@@ -1,57 +1,173 @@
-from tkinter import Tk
-from tkinter import Label
-from tkinter import ttk
-from tkinter import IntVar
-from tkinter import Checkbutton
-from tkinter import Button
-from tkinter import Entry
-from tkinter import filedialog
+# Importaciones externas
+from tkinter import Tk, Label, ttk, Button, Entry, filedialog
+from threading import Thread
+import os
+
+# Importaciones internas
 from down import download
-# AnotherGenericYoutubeDownloader (AGYD)
-# si no se coloca ruta se usa la del programa, si no se declara play list solo se baja una
-def res(): # existe para ser llamado por el submit y diversos tratos
-    pre_formato = "" # precacheo para:
-    if (video.get() == 0): # que si el boton de "video" no esta maracado entonces tomo en cuenta el formato de audio, si no entonces significa que es un video, por lo que paso el formato vacio para que al momento de la descarga se detecte que es un video (logica interna)
-        pre_formato = formato.get()
 
-    download(urlTP.get(), rutaT.cget("text"), video.get(),pre_formato, calidad.get())
-    pre_formato=""
-    #se hace video.get() a una variable a pesar de no ser el objeto ya que IntVar() es la forma en la que "es en lo q esta progamado Tkinter", asi q accedes al comportamiento del objeto al designarle un destino explisito
+# -- MANEJO DE LOG
+def log_on_startup():
+    # Si existe un log previo se elimina
+    if os.path.exists("out.txt"):
+        os.remove("out.txt")
+    # Si por alguna razon, no existia un log previo, se notifica por si acaso
+    else:
+        print("The file does not exist")
+    # Se crea el archivo para el log
+    open("out.txt", "x")
 
+"""
+    -----------------------
+    Diccionario de configuracion para down.py, ergo, ydl_opts
+    Orden en download(): url, ruta, video, formato, calidad
+    -----------------------
+"""
+config = {
+    # La ruta tomara por defecto el donde se este ejecutando AGYD
+    "ruta": os.getcwd(),
+    "is_video": 0,
+    "formato": "mp3",
+    "calidad_general": 1,
+    #'video_calidad': 1,
+    #'audio_calidad': 1,
+}
+
+"""
+    -----------------------
+    Buttons
+      and
+    Behavior
+    Fuctions for AGYD GUI
+    -----------------------
+"""
+
+
+# -- Function associated with the download button
+def down():
+    """
+    Se declaran los procedimientos por hilos por ser tareas tardadas,
+    lo que previene que el GUI del AGYD se congele
+    """
+    # Seteo del hilo de descarga
+    global trhead_download
+    trhead_download = Thread(
+        target=download,
+        args=(
+            urlTP.get(),
+            config["ruta"],  # SI NO se coloca ruta se usa la del programa
+            config["is_video"],
+            config["formato"],
+            config["calidad_general"],
+        ),
+    )
+
+    # Seteo del hilo de la barra de progreso
+    global thread_progress
+    thread_progress = Thread(
+        target=check_progress,
+    )  # Se declara el hilo a usar para la descarga, previniendo de que el GUI se congele
+
+    # Arranque de hilos
+    trhead_download.start()
+    thread_progress.start()
+
+
+# -- DEFAULTS PRESET FUNCTIONS
+# Default Audio Preset
+def audio_default():
+    config.update(formato="mp3")
+    config.update(is_video=0)
+    what_is_downloading.config(text="Audio")
+
+
+# Default Video Preset
+def video_default():
+    config.update(formato="mp4")
+    config.update(is_video=1)
+    what_is_downloading.config(text="Video")
+
+
+# -- PROGRESS BAR BEHAVIOR
+# Fuction used on thread declaration on down() fuction from Submmit Button
+def check_progress():
+    start_progress()
+    trhead_download.join()
+    stop_progress()
+
+
+# Start "Animation" for progress bar
+def start_progress():
+    progress.start()
+    progress.state = "disabled"
+    progress.state = "normal"
+
+
+# Stop "Animation" for progress bar
+def stop_progress():
+    progress.stop()
+    progress.state = "normal"
+    progress.state = "disabled"
+
+
+# -- Path desired function
 def path_finder():
-    rutaT.config(text= filedialog.askdirectory(
-        title = "Elija la ruta de descarga"        
-        )
-    ) # config es como para reescribir los parametros, q loco
+    # Declaramos la ruta elejida para guardarla
+    ruta_elejida = ""
+    # Guardamos la ruta en la variable al usar "filedialog"
+    ruta_elejida = filedialog.askdirectory(title="Elija la ruta de descarga")
+    # Si se elije una ruta
+    if ruta_elejida != "":
+        rutaT.config(text=ruta_elejida)
+        config.update(ruta=ruta_elejida)
+    else:  # Si NO se elije una ruta
+        # Reescribimos la ruta, tomando la locacion de ejecucion del AGYD
+        ruta_elejida = os.getcwd()
+        rutaT.config(text=ruta_elejida)
+        config.update(ruta=ruta_elejida)
 
+
+# -- Visual structure of AnotherGenericYoutubeDownloader (AGYD)
 main = Tk()
-main.title('AGYD')
-Label(main, text = "URL").grid(row=0)
-Label(main, text = "Video").grid(row=1)
-Label(main, text = "DEF: Audio").grid(row=1, column=2)
-Label(main, text = "Formato").grid(row=2)
-Label(main, text = "DEF: m4a (Audio) / mp4 (Video)").grid(row=2, column=2)
-Label(main, text = "Ruta").grid(row=3)
-rutaT = Label(main, text = "/././.")
-rutaT.grid(row=3,column=2) # se coloca aparte pq si no es como si no tuviera ubicacion, entonces su ubicacion relativa queda como "None", por lo q no hay sujeto
-Label(main, text = "Calidad").grid(row=4)
-Label(main, text = "DEF: Alta").grid(row=4, column=2)
+main.title("AGYD")
 
-formato = ttk.Combobox(main, values = ['m4a'], state = 'readonly') # Podria ser el codec
-formato.grid(row=2, column = 1)
-calidad = ttk.Combobox(main, values = ['Alta','Baja'], state = 'readonly')
-calidad.grid(row=4, column = 1)
-
-video = IntVar()# Container
-videoC = Checkbutton(main, text = '',variable = video)
-videoC.grid(row = 1, column = 1) # Presentacion
-
-pathB = Button(main, text = "Buscar", command = path_finder).grid(row = 3, column = 1)
-
+# Seccion URL
+Label(main, text="URL").grid(row=0, column=0)
 urlTP = Entry(main)
-urlTP.grid(row = 0, column=1)
+urlTP.grid(row=0, column=1)
 
-submit = Button(main, text = "Descargar", command = res) # sin parentesis pq si no se considera un llamado injectado
-submit.grid(row = 5)
+# Botones defaults
+# Seccion Audio Default Preset
+audio_default_button = Button(main, text="Audio Default Preset", command=audio_default)
+audio_default_button.grid(row=0, column=2)
 
+# Seccion Video Default Preset
+video_default_button = Button(main, text="Video Default Preset", command=video_default)
+video_default_button.grid(row=0, column=3)
+
+# Seccion RUTA
+Label(main, text="Ruta").grid(row=1, column=0)
+rutaT = Label(main, text=os.getcwd())  # Por defecto, mostrara la ruta del AGYD
+rutaT.grid(row=1, column=1)
+pathB = Button(main, text="Buscar", command=path_finder)
+pathB.grid(row=1, column=2)
+
+# Seccion SUBMIT
+submit = Button(
+    main, text="Descargar", command=down
+)  # sin parentesis pq si no se considera un llamado injectado
+submit.grid(row=2, column=1)
+
+# Seccion Feedback User what_is_downloading
+what_is_downloading = Label(main, text="Audio")
+what_is_downloading.grid(row=2, column=2)
+
+# Seccion BARRA DE PROGRESO
+progress = ttk.Progressbar(main, length=200, mode="indeterminate")
+progress.grid(row=3, column=2)
+
+# Ejecucion del GUI
 main.mainloop()
+
+# Se crea el log de AGYD 
+log_on_startup()
