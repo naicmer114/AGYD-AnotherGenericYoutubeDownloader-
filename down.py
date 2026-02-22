33c0c5300948d901f8f576c0cdf92502bc2b1bf3
naicmer114 <sergiojoseriveromarquez@gmail.com>
2026-02-23 00:52:50 +0100

    Classes Fase
    Now both of the py's are in classes structure.
    The log system was eradicated due to the unviability by now. The code of it could be used in future updates for debugging measures.
    The GUI was reconfigured and “stylish,” but it will change for the next update.
    Now the GUI shows the speed, progress, and estimated time through download (due to the addition of a redirection of the hooks from ytdlp).
    Now the URL (or text in general) can be pasted and copied from/to the clipboard.
    
    Acotations:
    * The redirection of the stdout of the system is not used due to the fact that yt-dlp internally redirects it, so it brings up an error not worth it.

--- down.py
+++ down.py
@@ -1,86 +1,130 @@
+# Importaciones externas
 from yt_dlp import YoutubeDL
-
+from win10toast import ToastNotifier
 import os
+
+# stdout t
 import sys
+import re
 
 
-# Search resources function
-def resource_path(name):
-    if hasattr(sys, "_MEIPASS"):
-        return os.path.join(sys._MEIPASS, name)
-    return os.path.join(os.path.abspath("."), name)
+class download:
+    def __init__(self, url, ruta, is_video, formato, calidad_general, label):
 
+        # Se declaran las rutas de ffmpeg y ffprobe
+        self.ffmpeg_dir = os.path.dirname(self.resource_path("ffmpeg.exe"))
+        self.ffprobe_dir = os.path.dirname(self.resource_path("ffprobe.exe"))
+        # print("ffmpeg:", ffmpeg_dir, os.path.exists(ffmpeg_dir))
+        # print("ffprobe:", ffprobe_dir, os.path.exists(ffprobe_dir))
+        # print("PATH:", os.environ["PATH"])
 
-# Se declaran las rutas de ffmpeg y ffprobe
-ffmpeg_dir = os.path.dirname(resource_path("ffmpeg.exe"))
-ffprobe_dir = os.path.dirname(resource_path("ffprobe.exe"))
-# print("ffmpeg:", ffmpeg_dir, os.path.exists(ffmpeg_dir))
-# print("ffprobe:", ffprobe_dir, os.path.exists(ffprobe_dir))
-# print("PATH:", os.environ["PATH"])
+        # Lists of quality presets
+        self.Quality = ["worst", "best"]
+        self.Quality_list_for_audio = ["worstaudio", "bestaudio"]
+        self.Quality_list_for_video = ["worstvideo", "bestvideo"]
 
+        # Visual Misc
+        self.toaster = ToastNotifier()
 
-# Lists of quality presets
-Quality = ["worst", "best"]
-Quality_list_for_audio = ["worstaudio", "bestaudio"]
-Quality_list_for_video = ["worstvideo", "bestvideo"]
+        # -- Declaro la variable para posterior uso
+        self.Quality_global = self.Quality[calidad_general]
+        self.Quality_audio = ""
+        self.Quality_vid = ""
 
+        # -- PROCESADO DE ydl_opts
+        # PARA AUDIO, con inyection de portadas y metadata
+        if is_video == 0:
+            self.Quality_audio = self.Quality_list_for_audio[calidad_general]
+            self.ydl_opts = {
+                "format": self.Quality_global,
+                "progress_hooks": [
+                    self.progress_hook_factory(self.gui_callback, label)
+                ],
+                "outtmpl": ruta + "/Track: %(playlist_index)s - %(title)s.%(ext)s",
+                "quiet": False,  # Desactiva el output a consola
+                "ffmpeg_location": self.ffmpeg_dir,
+                "postprocessors": [  # Extract audio using ffmpeg
+                    {
+                        # Separacion de audio del video para posprocesado
+                        "key": "FFmpegExtractAudio",
+                        "preferredcodec": formato,  # Codec de audio/video a usar
+                        "preferredquality": "320",
+                        "nopostoverwrites": False,
+                    },
+                    {
+                        # Se incorpora la metadata de la cancion.
+                        # Se hace primero ya que la portada se pierde al reescribir
+                        # la metadata, mas no al revez
+                        "key": "FFmpegMetadata"
+                    },
+                    {"key": "EmbedThumbnail"},  # Se incorpora la portada al archivo
+                ],
+                "writethumbnail": True,  # Se baja la portada
+                "verbose": True,
+            }
 
-def download(url, ruta, is_video, formato, calidad_general):
-    # -- Declaro la variable para posterior uso
-    Quality_global = Quality[calidad_general]
-    Quality_audio = ""
-    Quality_vid = ""
+        # PARA VIDEO, con inyection de portadas y metadata
+        if is_video == 1:
+            self.Quality_audio = self.Quality_list_for_audio[calidad_general]
+            self.Quality_vid = self.Quality_list_for_video[calidad_general]
+            self.ydl_opts = {
+                "progress_hooks": [
+                    self.progress_hook_factory(self.gui_callback, label)
+                ],
+                "format": formato + "/" + self.Quality_audio + "/" + self.Quality_vid,
+                "outtmpl": ruta + "/%(title)s.%(ext)s",
+                "quiet": False,  # Desactiva el output a consola
+                "ffmpeg_location": os.path.realpath("./libs/ffmpeg/bin/ffmpeg.exe"),
+            }
 
-    # -- PROCESADO DE ydl_opts
-    # PARA AUDIO, con inyection de portadas y metadata
-    if is_video == 0:
-        Quality_audio = Quality_list_for_audio[calidad_general]
-        ydl_opts = {
-            "format": Quality_global,
-            "outtmpl": ruta + "/Track: %(playlist_index)s - %(title)s.%(ext)s",
-            "quiet": True,  # Desactiva el output a consola
-            "ffmpeg_location": ffmpeg_dir,
-            "postprocessors": [  # Extract audio using ffmpeg
-                {
-                    # Separacion de audio del video para posprocesado
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": formato,  # Codec de audio/video a usar
-                    "preferredquality": "320",
-                    "nopostoverwrites": False,
-                },
-                {
-                    # Se incorpora la metadata de la cancion.
-                    # Se hace primero ya que la portada se pierde al reescribir
-                    # la metadata, mas no al revez
-                    "key": "FFmpegMetadata"
-                },
-                {"key": "EmbedThumbnail"},  # Se incorpora la portada al archivo
-            ],
-            "writethumbnail": True,  # Se baja la portada
-            "verbose": True,
-        }
-
-    # PARA VIDEO, con inyection de portadas y metadata
-    if is_video == 1:
-        Quality_audio = Quality_list_for_audio[calidad_general]
-        Quality_vid = Quality_list_for_video[calidad_general]
-
-        ydl_opts = {
-            "format": formato + "/" + Quality_audio + "/" + Quality_vid,
-            "outtmpl": ruta + "/Track: %(playlist_index)s - %(title)s.%(ext)s",
-            "quiet": True,  # Desactiva el output a consola
-            "ffmpeg_location": os.path.realpath("./libs/ffmpeg/bin/ffmpeg.exe"),
-        }
-
-    org_std = sys.stdout  # save the original sys.stdout to reset it
-    with open("out.txt", "a") as f:
-        sys.stdout = f  # redirect stdout to direct write
         """
-            Usando el metodo YoutubeDL y dandole las configuraciones previas
-            como parametros, se emplea su comportameniento de descargar
-            (Download) dandole el url del video / playlist
+            --- Dowload Section ---
         """
+
         print("> Descargando")
-        print(ydl_opts)
-        YoutubeDL(ydl_opts).download(url)
-    sys.stdout = org_std  # reset the stdout as original as it was
+        print(self.ydl_opts)
+        YoutubeDL(self.ydl_opts).download(url)
+
+        # Se muestra la notificacion
+        self.toaster.show_toast(
+            "AGYD", "Descarga/s terminada/s", icon_path="AGYD_logo.ico", duration=5
+        )
+
+        print("> Hilo terminado")
+
+    # Search resources function
+    def resource_path(self, name):
+        if hasattr(sys, "_MEIPASS"):
+            return os.path.join(sys._MEIPASS, name)
+        return os.path.join(os.path.abspath("."), name)
+
+    # Procesos asociados a la redireccion de la informacion de ytdlp
+    def progress_hook_factory(self, gui_callback, label):
+        # Procesamos la redireccion de la informacion sobre el progreso de ytdlp
+        def hook(d):
+            # Si esta descargando procesamos y presentamos la info en el gui
+            if d["status"] == "downloading":
+                # Se toman los datos y se quitan espacios vacios
+                percent = d.get("_percent_str", "").strip()
+                speed = d.get("_speed_str", "").strip()
+                eta = d.get("_eta_str", "").strip()
+                # Se reconstruye el texto a presentar bajo el formato deseado
+                texto = f"{percent} {speed}\nTiempo faltante: {eta}"
+
+                # Limpiamos los códigos ANSI (caracteres invalidos) antes de mostrar
+                texto_limpio = self.limpiar_ansi(texto)
+                # Pasamos la informacion a la funcion que modificara el gui
+                self.gui_callback(texto_limpio, label)
+
+        return hook
+
+    # Limpiamos el texto que se pase de caracteres invialidos para la presentacion
+    def limpiar_ansi(self, texto):
+        # Regex que elimina todos los códigos de color ANSI
+        ansi_escape = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")
+        return ansi_escape.sub("", texto)  # Se devuelven los datos sin comillas
+
+    # Se actualizan los datos de la gui e nbase a los datos disponibles
+    def gui_callback(self, texto, label):
+        # Se modifica el Label del gui que muestra la velocidad de descarga
+        label.config(text=texto)
--- front.py
+++ front.py
@@ -1,173 +1,299 @@
 # Importaciones externas
-from tkinter import Tk, Label, ttk, Button, Entry, filedialog
+import tkinter as tk
+from tkinter import ttk, filedialog
 from threading import Thread
 import os
 
 # Importaciones internas
 from down import download
 
-# -- MANEJO DE LOG
-def log_on_startup():
-    # Si existe un log previo se elimina
-    if os.path.exists("out.txt"):
-        os.remove("out.txt")
-    # Si por alguna razon, no existia un log previo, se notifica por si acaso
-    else:
-        print("The file does not exist")
-    # Se crea el archivo para el log
-    open("out.txt", "x")
 
-"""
-    -----------------------
-    Diccionario de configuracion para down.py, ergo, ydl_opts
-    Orden en download(): url, ruta, video, formato, calidad
-    -----------------------
-"""
-config = {
-    # La ruta tomara por defecto el donde se este ejecutando AGYD
-    "ruta": os.getcwd(),
-    "is_video": 0,
-    "formato": "mp3",
-    "calidad_general": 1,
-    #'video_calidad': 1,
-    #'audio_calidad': 1,
-}
+class window(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        # --- Inicializacion de datos ---
+        """
+        -----------------------
+        Diccionario de configuracion para down.py, ergo, ydl_opts
+        Orden en download(): url, ruta, video, formato, calidad
+        -----------------------
+        """
+        self.download_config = {
+            # La ruta tomara por defecto el donde se este ejecutando AGYD
+            "ruta": os.getcwd(),
+            "is_video": 0,
+            "formato": "mp3",
+            "calidad_general": 1,
+            #'video_calidad': 1,
+            #'audio_calidad': 1,
+        }
 
-"""
-    -----------------------
-    Buttons
-      and
-    Behavior
-    Fuctions for AGYD GUI
-    -----------------------
-"""
+        """
+            --- Tratado de tkinter --- 
+        """
+        ttk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
 
+        # Se establece el titulo de la ventana
+        parent.title("AGYD")
 
-# -- Function associated with the download button
-def down():
+        parent.resizable(False, False)
+        # Se establece el icono de la ventana
+        # parent.iconbitmap("AGYD_logo.ico")
+
+        """
+            --- Static Style Section ---
+        """
+        # Se abrevia el ttk.Style() en una variable
+        style = ttk.Style()
+        # Estilo en general configurado
+        style.theme_use("clam")  # Try: 'alt', 'default', 'classic', 'clam'
+
+        # Color/Fondo del frame y ventana
+        bg_color_GENERAL = "#fcff7d"
+        self.parent.config(bg=bg_color_GENERAL)
+        style.configure(
+            "TFrame",
+            background=bg_color_GENERAL,
+        )
+        # Color/Fondo de los botones de la ventana
+        bg_button_color_GENERAL = "#807dff"
+        bg_button_color_FOCUSED = "#adacff"
+        style.configure(
+            "TButton",
+            relief="ridge",
+            background=bg_button_color_GENERAL,
+            foreground="black",
+            focusthickness=3,
+            focuscolor="none",
+            font=("Segoe UI", 10, "bold"),
+        )
+        style.configure(
+            "TLabel",
+            background=bg_color_GENERAL,
+            font=("Segoe UI", 10, "bold"),
+        )
+
+        """
+            --- Interactive Style Section --- 
+        """
+        style.map(
+            "TButton",
+            background=[("active", bg_button_color_FOCUSED)],
+            relief=[("active", "solid")],
+        )
+        style.map("TEntry", lightcolor=[("focus", "blue")])
+
+        # Seccion URL
+        ttk.Label(self, text="URL").grid(row=0, column=0)
+        self.urlTP = ttk.Entry(self, width=50)
+        self.urlTP.grid(row=0, column=1)
+
+        # Seccion RUTA
+        ttk.Label(self, text="Ruta de descarga / Download path").grid(row=1, column=0)
+        self.rutaT = ttk.Label(
+            self, text=os.getcwd()
+        )  # Por defecto, mostrara la ruta del AGYD
+        self.rutaT.grid(row=1, column=1)
+        self.pathB = ttk.Button(self, text="Buscar / Browse", command=self.path_finder)
+        self.pathB.grid(row=1, column=2)
+
+        # -- Botones defaults
+
+        # Label de los presets
+        ttk.Label(self, text="Presets para descarga / Download Preset").grid(
+            row=2, column=0
+        )
+
+        # Seccion Audio Default Preset
+        self.audio_default_button = ttk.Button(
+            self, text="Audio Default Preset", command=self.audio_default
+        )
+        self.audio_default_button.grid(row=2, column=1)
+
+        # Seccion Video Default Preset
+        self.video_default_button = ttk.Button(
+            self, text="Video Default Preset", command=self.video_default
+        )
+        self.video_default_button.grid(row=2, column=2)
+
+        # Seccion Feedback User what_is_downloading
+        self.preset_selected = ttk.Label(self, text="Preset selected / Preset elegido")
+        self.preset_selected.grid(row=3, column=0)
+
+        # Seccion Feedback User what_is_downloading
+        self.what_is_downloading = ttk.Label(self, text="Audio")
+        self.what_is_downloading.grid(row=3, column=1)
+
+        # Seccion SUBMIT
+        self.submit = ttk.Button(
+            self, text="Descargar / Download", command=self.down
+        )  # sin parentesis pq si no se considera un llamado injectado
+        self.submit.grid(row=4, column=1)
+
+        # Temp Reminder
+        self.reminder = ttk.Label(
+            self,
+            text="--------------------------------------------------------->\n"
+            + "(ENG: While downloading, wait for this boy to be static :3)\n"
+            + "(ESP:Durante descargas, espera a que la barra este estatica :3)",
+        )
+        self.reminder.grid(row=5, column=0)
+
+        # Seccion BARRA DE PROGRESO
+        self.progress = ttk.Progressbar(self, length=200, mode="indeterminate")
+        self.progress.grid(row=5, column=1)
+
+        # LABELS FOR DOWNLOAD FEEDBACK
+        self.speed = ttk.Label(
+            self,
+            text="ENG: Download speed, percentage and estimated time\n"
+            + "ESP: Velocidad de descarga, porcentaje y tiempo estimado",
+        )
+        self.speed.grid(row=5, column=2)
+
+        """
+            Context Menu
+            
+            Thanks to Delrius Euphoria from stack overflow
+            Answer Associated: https://stackoverflow.com/a/66514657
+        """
+        # Se declara el menu contextual con ayuda de Tkinter
+        self.menu = tk.Menu(self, tearoff=0)
+        # Se añaden las opciones de dicho menu contextual
+        self.menu.add_command(label="Paste / Pegar", command=self.contextM_paste)
+        self.menu.add_command(label="Copy / Copiar", command=self.contextM_copy)
+        # Se asocia los comportamientos ocurridos con el menu contextual
+        # A la bandeja de texto
+        self.urlTP.bind("<Button-3>", self.popup)
+
     """
-    Se declaran los procedimientos por hilos por ser tareas tardadas,
-    lo que previene que el GUI del AGYD se congele
+        -----------------------
+            Behavior
+        Fuctions for AGYD GUI
+        -----------------------
     """
-    # Seteo del hilo de descarga
-    global trhead_download
-    trhead_download = Thread(
-        target=download,
-        args=(
-            urlTP.get(),
-            config["ruta"],  # SI NO se coloca ruta se usa la del programa
-            config["is_video"],
-            config["formato"],
-            config["calidad_general"],
-        ),
-    )
 
-    # Seteo del hilo de la barra de progreso
-    global thread_progress
-    thread_progress = Thread(
-        target=check_progress,
-    )  # Se declara el hilo a usar para la descarga, previniendo de que el GUI se congele
+    # -- Function associated with the download button
+    def down(self):
+        """
+        Se declaran los procedimientos por hilos por ser tareas tardadas,
+        lo que previene que el GUI del AGYD se congele
+        """
 
-    # Arranque de hilos
-    trhead_download.start()
-    thread_progress.start()
+        # Seteo del hilo de descarga
+        global trhead_download
+        trhead_download = Thread(
+            target=download,
+            args=(
+                self.urlTP.get(),
+                self.download_config[
+                    "ruta"
+                ],  # SI NO se coloca ruta se usa la del programa
+                self.download_config["is_video"],
+                self.download_config["formato"],
+                self.download_config["calidad_general"],
+                self.speed,
+            ),
+        )
+
+        # Seteo del hilo de la barra de progreso
+        global thread_progress
+        thread_progress = Thread(
+            target=self.check_progress,
+        )  # Se declara el hilo a usar para la descarga, previniendo de que el GUI se congele
+
+        # Arranque de hilos
+        # No se declaran en self pq son hinerentes de la funcion "down" en la que estan
+        trhead_download.start()
+        thread_progress.start()
+
+    # -- Path desired function
+    def path_finder(self):
+        # Declaramos la ruta elejida para guardarla
+        ruta_elejida = ""
+        # Guardamos la ruta en la variable al usar "filedialog"
+        ruta_elejida = filedialog.askdirectory(title="Elija la ruta de descarga")
+        # Si se elije una ruta
+        if ruta_elejida != "":
+            self.rutaT.config(text=ruta_elejida)
+            self.download_config.update(ruta=ruta_elejida)
+        else:  # Si NO se elije una ruta
+            # Reescribimos la ruta, tomando la locacion de ejecucion del AGYD
+            ruta_elejida = os.getcwd()
+            self.rutaT.config(text=ruta_elejida)
+            self.download_config.update(ruta=ruta_elejida)
+
+    # -- DEFAULTS PRESET FUNCTIONS
+    # Default Audio Preset
+    def audio_default(self):
+        self.download_config.update(formato="mp3")
+        self.download_config.update(is_video=0)
+        self.what_is_downloading.config(text="Audio")
+
+    # Default Video Preset
+    def video_default(self):
+        self.download_config.update(formato="mp4")
+        self.download_config.update(is_video=1)
+        self.what_is_downloading.config(text="Video")
+
+    # -- PROGRESS BAR BEHAVIOR
+    # Fuction used on thread declaration on down() fuction from Submmit Button
+    def check_progress(self):
+        self.start_progress()
+        trhead_download.join()
+        self.stop_progress()
+
+    # Start "Animation" for progress bar
+    def start_progress(self):
+        self.progress.start()
+        self.progress.state = "disabled"
+        self.progress.state = "normal"
+
+    # Stop "Animation" for progress bar
+    def stop_progress(self):
+        self.progress.stop()
+        self.progress.state = "normal"
+        self.progress.state = "disabled"
+
+    """
+        Context Menu Behavior
+    """
+
+    def popup(self, event):
+        try:
+            self.menu.tk_popup(
+                event.x_root, event.y_root
+            )  # Pop the menu up in the given coordinates
+        finally:
+            self.menu.grab_release()
+
+    def contextM_paste(self):
+        # Se toma lo que este en el porta papeles
+        clipboard = root.clipboard_get()
+        # Se limpia el campo de texto
+        self.urlTP.delete(0, tk.END)
+        # Se pega en el campo de texto el lo que este en el porta papeles
+        self.urlTP.insert("end", clipboard)
+
+    def contextM_copy(self):
+        # Se guarda el texto que haya en la bandeja de texto
+        text_being_in_entry = self.urlTP.get()
+        # Se limpia el porta papeles
+        root.clipboard_clear()
+        # Se copia al porta papeles el texto que habia en la bandeja
+        root.clipboard_append(text_being_in_entry)
 
 
-# -- DEFAULTS PRESET FUNCTIONS
-# Default Audio Preset
-def audio_default():
-    config.update(formato="mp3")
-    config.update(is_video=0)
-    what_is_downloading.config(text="Audio")
+# Se define la forma de proceder inicial al llamar al front
+if __name__ == "__main__":
+    # Se declara al root como empleador de los modulos de tkinter
+    root = tk.Tk()
 
+    # Se llama a la clase para crear el objeto de ventana,
+    # y se empaquetan los parametros de heredara la ventana
+    window(root).pack(side="top", fill="both", expand=True)
 
-# Default Video Preset
-def video_default():
-    config.update(formato="mp4")
-    config.update(is_video=1)
-    what_is_downloading.config(text="Video")
+    # Se usa una verison extremadamente simplificada para centrar la ventana
+    root.eval("tk::PlaceWindow . center")
 
-
-# -- PROGRESS BAR BEHAVIOR
-# Fuction used on thread declaration on down() fuction from Submmit Button
-def check_progress():
-    start_progress()
-    trhead_download.join()
-    stop_progress()
-
-
-# Start "Animation" for progress bar
-def start_progress():
-    progress.start()
-    progress.state = "disabled"
-    progress.state = "normal"
-
-
-# Stop "Animation" for progress bar
-def stop_progress():
-    progress.stop()
-    progress.state = "normal"
-    progress.state = "disabled"
-
-
-# -- Path desired function
-def path_finder():
-    # Declaramos la ruta elejida para guardarla
-    ruta_elejida = ""
-    # Guardamos la ruta en la variable al usar "filedialog"
-    ruta_elejida = filedialog.askdirectory(title="Elija la ruta de descarga")
-    # Si se elije una ruta
-    if ruta_elejida != "":
-        rutaT.config(text=ruta_elejida)
-        config.update(ruta=ruta_elejida)
-    else:  # Si NO se elije una ruta
-        # Reescribimos la ruta, tomando la locacion de ejecucion del AGYD
-        ruta_elejida = os.getcwd()
-        rutaT.config(text=ruta_elejida)
-        config.update(ruta=ruta_elejida)
-
-
-# -- Visual structure of AnotherGenericYoutubeDownloader (AGYD)
-main = Tk()
-main.title("AGYD")
-
-# Seccion URL
-Label(main, text="URL").grid(row=0, column=0)
-urlTP = Entry(main)
-urlTP.grid(row=0, column=1)
-
-# Botones defaults
-# Seccion Audio Default Preset
-audio_default_button = Button(main, text="Audio Default Preset", command=audio_default)
-audio_default_button.grid(row=0, column=2)
-
-# Seccion Video Default Preset
-video_default_button = Button(main, text="Video Default Preset", command=video_default)
-video_default_button.grid(row=0, column=3)
-
-# Seccion RUTA
-Label(main, text="Ruta").grid(row=1, column=0)
-rutaT = Label(main, text=os.getcwd())  # Por defecto, mostrara la ruta del AGYD
-rutaT.grid(row=1, column=1)
-pathB = Button(main, text="Buscar", command=path_finder)
-pathB.grid(row=1, column=2)
-
-# Seccion SUBMIT
-submit = Button(
-    main, text="Descargar", command=down
-)  # sin parentesis pq si no se considera un llamado injectado
-submit.grid(row=2, column=1)
-
-# Seccion Feedback User what_is_downloading
-what_is_downloading = Label(main, text="Audio")
-what_is_downloading.grid(row=2, column=2)
-
-# Seccion BARRA DE PROGRESO
-progress = ttk.Progressbar(main, length=200, mode="indeterminate")
-progress.grid(row=3, column=2)
-
-# Ejecucion del GUI
-main.mainloop()
-
-# Se crea el log de AGYD 
-log_on_startup()
+    root.mainloop()
